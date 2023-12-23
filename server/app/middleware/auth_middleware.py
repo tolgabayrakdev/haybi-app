99e6e9962f30c880265b1f49fc6db0c80e81f4b8
tolgabayrakdev <bayraktolga28@gmail.com>
2023-12-23 10:54:05 +0300

    auth middlewate

---
+++ server/app/middleware/auth_middleware.py
@@ -0,0 +1,23 @@
+from fastapi import Request, HTTPException
+from ..database import SessionLocal
+from ..model import User
+import jwt
+
+
+async def auth_middleware(request: Request):
+    # HTTP taleplerinden access tokenı alıyoruz
+    access_token = request.cookies.get("access_token")
+    if access_token:
+        # Token varsa, kullanıcı kimliğini doğrulayalım
+        try:
+            payload = jwt.decode(access_token, "secret_key", algorithms=["HS256"])
+            print(payload)
+            user_id = int(payload.get("payload"))
+            db = SessionLocal()
+            user = db.query(User).filter(User.id == user_id).first()
+            if not user:
+                raise HTTPException(status_code=401, detail="Invalid token")
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail="Token has expired")
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=403, detail="Invalid token")
